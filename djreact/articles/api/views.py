98dc3618a8a850a15033564474f9dd05c2f655df
Alison Reilly <reillyamr@gmail.com>
2020-08-12 16:30:58 -0400

    change to auths version,incl correct short exp

--- djreact/articles/api/views.py
+++ djreact/articles/api/views.py
@@ -1,57 +1,50 @@
-#change method for basic, unauthed get, put, patch, destroy requests
-#uses less lines of code, keeping the other version for use when auths get involved in tutorial
-#methods below, instead, to use all views in one
-from rest_framework import viewsets
-
-#this part stays thes same
+from rest_framework import permissions
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+    UpdateAPIView
+)
 from articles.models import Article
 from .serializers import ArticleSerializer
 
+class ArticleListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.AllowAny, )
 
 
-class ArticleViewSet(viewsets.ModelViewSet):
-        queryset = Article.objects.all()
-        serializer_class = ArticleSerializer
+class ArticleDetailView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.AllowAny, )
 
 
-#this part is for permissions used later and not necc as viewsets currently stands
-#from rest_framework import permissions
+class ArticleCreateView(CreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
-#this is from the method used later for auths
-# from rest_framework.generics import (
-#     ListAPIView,
-#     RetrieveAPIView,
-#     CreateAPIView,
-#     DestroyAPIView,
-#     UpdateAPIView
-# )
 
-#methods below using each specific type of View
-# class ArticleListView(ListAPIView):
-#     queryset = Article.objects.all()
+class ArticleUpdateView(UpdateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+
+class ArticleDeleteView(DestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+#previous version when auths not needed:
+
+# from rest_framework import viewsets
+
+# from articles.models import Article
+# from .serializers import ArticleSerializer
+
+# class ArticleViewSet(viewsets.ModelViewSet):
 #     serializer_class = ArticleSerializer
-#     permission_classes = (permissions.AllowAny, )
-
-
-# class ArticleDetailView(RetrieveAPIView):
 #     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializer
-#     permission_classes = (permissions.AllowAny, )
-
-
-# class ArticleCreateView(CreateAPIView):
-#     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializer
-#     permission_classes = (permissions.IsAuthenticated, )
-
-
-# class ArticleUpdateView(UpdateAPIView):
-#     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializer
-#     permission_classes = (permissions.IsAuthenticated, )
-
-
-# class ArticleDeleteView(DestroyAPIView):
-#     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializer
-#     permission_classes = (permissions.IsAuthenticated, )
