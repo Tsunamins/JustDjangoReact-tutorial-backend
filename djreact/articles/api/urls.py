4bcca2435ef3381e43b80bc9eaf41805adb88b23
Alison Reilly <reillyamr@gmail.com>
2020-08-12 16:00:47 -0400

    add addtl views and clean up code

--- djreact/articles/api/urls.py
+++ djreact/articles/api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from .views import ArticleListView, ArticleDetailView
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView
+)
 
-# the <pk> path allows articles/1, have seen other examples, but this is more specific
+#when viewing create have to put in base/api/create/ bc of / below
 
 urlpatterns = [
     path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view())
+    path('create/', ArticleCreateView.as_view()),
+    path('<pk>', ArticleDetailView.as_view()),
+    path('<pk>/update/', ArticleUpdateView.as_view()),
+    path('<pk>/delete/', ArticleDeleteView.as_view())
 ]
