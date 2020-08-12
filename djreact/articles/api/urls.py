b5f76852feb82b28699590ea93fb0fb846697817
Alison Reilly <reillyamr@gmail.com>
2020-08-12 16:25:08 -0400

    urls-match short viewset, no wrk ,kept chg as exp

--- djreact/articles/api/urls.py
+++ djreact/articles/api/urls.py
@@ -1,19 +1,35 @@
-from django.urls import path
+from articles.api.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
-from .views import (
-    ArticleListView,
-    ArticleDetailView,
-    ArticleCreateView,
-    ArticleUpdateView,
-    ArticleDeleteView
-)
+router = DefaultRouter()
+router.register(r'api', ArticleViewSet, base_name='articles')
+urlpatterns = router.urls
 
-#when viewing create have to put in base/api/create/ bc of / below
 
-urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('create/', ArticleCreateView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
-    path('<pk>/update/', ArticleUpdateView.as_view()),
-    path('<pk>/delete/', ArticleDeleteView.as_view())
-]
+
+
+
+
+
+
+
+
+# from django.urls import path
+
+# from .views import (
+#     ArticleListView,
+#     ArticleDetailView,
+#     ArticleCreateView,
+#     ArticleUpdateView,
+#     ArticleDeleteView
+# )
+
+# #when viewing create have to put in base/api/create/ bc of / below
+
+# urlpatterns = [
+#     path('', ArticleListView.as_view()),
+#     path('create/', ArticleCreateView.as_view()),
+#     path('<pk>', ArticleDetailView.as_view()),
+#     path('<pk>/update/', ArticleUpdateView.as_view()),
+#     path('<pk>/delete/', ArticleDeleteView.as_view())
+# ]
