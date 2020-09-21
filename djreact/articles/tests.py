9ce9d7c79ab0786708f797412686286e700f8718
Alison Reilly <reillyamr@gmail.com>
2020-09-21 12:09:08 -0400

    add model test that works

--- djreact/articles/tests.py
+++ djreact/articles/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.test import TestCase
+from .models import Article
+
 
 # Create your tests here.
+class ModelTestCase(TestCase):
+    """This class defines the test suite for the bucketlist model."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.article_title = "Write world class code"
+        self.article = Article(title=self.article_title)
+
+    def test_model_can_create_a_bucketlist(self):
+        """Test the bucketlist model can create a bucketlist."""
+        old_count = Article.objects.count()
+        self.article.save()
+        new_count = Article.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+
