193789cf13f6e6c128f91ba41b6a5fa2380e047d
Alison Reilly <reillyamr@gmail.com>
2020-09-21 14:07:00 -0400

    add successful get/delete/put endpoint tests

--- djreact/articles/tests.py
+++ djreact/articles/tests.py
@@ -4,6 +4,9 @@ from .models import Article
 
 from rest_framework.test import APIClient
 from rest_framework import status
+
+from rest_framework.test import force_authenticate
+
 # from django.core.urlresolvers import reverse
 from django.urls import reverse
 
@@ -32,6 +35,7 @@ class ViewTestCase(TestCase):
         """Define the test client and other test variables."""
         self.client = APIClient()
         self.article_data = {'title': 'Go to Ibiza', 'content': 'New article from tests'}
+     
         self.response = self.client.post('/api/create/', self.article_data, format='json')
         # self.assertEqual(response.status_code, 201)
 
@@ -43,3 +47,38 @@ class ViewTestCase(TestCase):
     def test_api_can_create_a_article(self):
         """Test the api has article creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_article(self):
+        """Test the api can get a given bucketlist."""
+        article = Article.objects.get()
+      
+        response = self.client.get('/api/{}'.format(article.id), format="json")
+        # reverse('details',
+        # kwargs={'pk': article.id}), format="json"
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, article)
+
+    def test_api_can_update_article(self):
+        """Test the api can update a given bucketlist."""
+        article = Article.objects.get()
+      
+        change_article = {'title': 'An article to update', 'content': 'Updated article content'}
+      
+        res = self.client.put('/api/{}/update/'.format(article.id), change_article, format="json")
+        #     reverse('details', kwargs={'pk': article.id}),
+        #     change_article, format='json'
+        # )
+      
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_article(self):
+        """Test the api can delete a bucketlist."""
+        article = Article.objects.get()
+      
+        response = self.client.delete('/api/{}/delete/'.format(article.id), format="json", follow=True)
+        # reverse('details', kwargs={'pk': article.id}),
+        # format='json',
+        # follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
