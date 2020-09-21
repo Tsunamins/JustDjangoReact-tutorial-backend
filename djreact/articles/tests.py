495bc05b082e3cd5d473aba7992cb0c7a33bbb33
Alison Reilly <reillyamr@gmail.com>
2020-09-21 12:50:24 -0400

    add successful create endpoint test

--- djreact/articles/tests.py
+++ djreact/articles/tests.py
@@ -2,6 +2,11 @@ from django.test import TestCase
 from django.test import TestCase
 from .models import Article
 
+from rest_framework.test import APIClient
+from rest_framework import status
+# from django.core.urlresolvers import reverse
+from django.urls import reverse
+
 
 # Create your tests here.
 class ModelTestCase(TestCase):
@@ -20,3 +25,21 @@ class ModelTestCase(TestCase):
         self.assertNotEqual(old_count, new_count)
 
 
+class ViewTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.article_data = {'title': 'Go to Ibiza', 'content': 'New article from tests'}
+        self.response = self.client.post('/api/create/', self.article_data, format='json')
+        # self.assertEqual(response.status_code, 201)
+
+        # self.response = self.client.post(
+        #     '/api/create/',
+        #     self.article_data,
+        #     format="json")
+
+    def test_api_can_create_a_article(self):
+        """Test the api has article creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
