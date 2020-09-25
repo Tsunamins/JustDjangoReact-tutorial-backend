1dda0d7486c7f2e679903288c3d2b12f4ae5de52
Alison Reilly <reillyamr@gmail.com>
2020-09-25 15:02:46 -0400

    add registration urls with email confirmation

--- djreact/articles/api/urls.py
+++ djreact/articles/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from django.conf.urls import url, include
+from allauth.account.views import ConfirmEmailView
+from . import views
+
 from .views import (
     ArticleListView,
     ArticleDetailView,
@@ -13,10 +17,16 @@ urlpatterns = [
     path('create/', ArticleCreateView.as_view()),
     path('<pk>', ArticleDetailView.as_view()),
     path('<pk>/update/', ArticleUpdateView.as_view()),
-    path('<pk>/delete/', ArticleDeleteView.as_view())
+    path('<pk>/delete/', ArticleDeleteView.as_view()),
+
+
+    path('registration/account-email-verification-sent/', views.null_view, name='account_email_verification_sent'),
+    path('registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
+    path('registration/complete/$', views.complete_view, name='account_confirm_complete'),
+    path('password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.null_view, name='password_reset_confirm'),
 ]
 
-#Previous short version for when auths aren't needed:
+# Previous short version for when auths aren't needed:
 
 # from articles.api.views import ArticleViewSet
 # from rest_framework.routers import DefaultRouter
