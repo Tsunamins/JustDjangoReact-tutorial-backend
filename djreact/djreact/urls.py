891761b0af090921a94bc70c173e3518dc7214ad
Alison Reilly <reillyamr@gmail.com>
2020-09-29 14:39:52 -0400

    add urls for djoser, chg naming of non-admin urls

--- djreact/djreact/urls.py
+++ djreact/djreact/urls.py
@@ -6,11 +6,11 @@ from django.urls import path, include
 # import emailsignup.urls
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('articles.api.urls')),
-    # path('auth/', emailsignup.urls),
+    path('non-admin/', include('rest_framework.urls')),
+
+    path('auth/', include('djoser.urls')),
+
+    path('api/', include('articles.api.urls')), 
 
 ]
