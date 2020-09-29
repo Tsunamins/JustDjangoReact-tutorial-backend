59607543ba034a140b8c2ef0ffe0cbca42751bac
Alison Reilly <reillyamr@gmail.com>
2020-09-29 14:39:10 -0400

    change urls to articles/,remove prior example urls

--- djreact/articles/api/urls.py
+++ djreact/articles/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from django.conf.urls import url, include
-from allauth.account.views import ConfirmEmailView
+
 from . import views
 
 from .views import (
@@ -10,19 +10,16 @@ from .views import (
     ArticleCreateView,
     ArticleUpdateView,
     ArticleDeleteView,
-   
+  
 )
 
 urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('create/', ArticleCreateView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
-    path('<pk>/update/', ArticleUpdateView.as_view()),
-    path('<pk>/delete/', ArticleDeleteView.as_view()),
-    path('registration/account-email-verification-sent/', views.null_view, name='account_email_verification_sent'),
-    path('registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
-    path('registration/complete/$', views.complete_view, name='account_confirm_complete'),
-    path('password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.null_view, name='password_reset_confirm'),
+    path('articles/', ArticleListView.as_view()),
+    path('articles/create/', ArticleCreateView.as_view()),
+    path('articles/<pk>', ArticleDetailView.as_view()),
+    path('articles/<pk>/update/', ArticleUpdateView.as_view()),
+    path('articles/<pk>/delete/', ArticleDeleteView.as_view()),
+  
 ]
 
 # Previous short version for when auths aren't needed:
