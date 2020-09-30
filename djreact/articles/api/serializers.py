94a0ea1e5ee546c43c4711e0e3a977c0406a641d
Alison Reilly <reillyamr@gmail.com>
2020-09-30 17:33:59 -0400

    whitespace and new changes based on djoser/jwt

--- djreact/articles/api/serializers.py
+++ djreact/articles/api/serializers.py
@@ -4,6 +4,7 @@ from articles.models import Article
 
 from django.contrib.auth.models import User
 
+
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
