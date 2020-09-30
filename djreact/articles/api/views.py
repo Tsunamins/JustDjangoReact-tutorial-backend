2ab03d13e597287d1c60666c96d99fab0e2cd1df
Alison Reilly <reillyamr@gmail.com>
2020-09-30 18:18:15 -0400

    add custom create user view for email purposes

--- djreact/articles/api/views.py
+++ djreact/articles/api/views.py
@@ -7,12 +7,37 @@ from rest_framework.generics import (
     UpdateAPIView
 )
 
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from articles.models import Article
-from .serializers import ArticleSerializer
+from django.contrib.auth.models import User
+from .serializers import ArticleSerializer, CreateUserSerializer
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+
+
+class CreateUserView(generics.GenericAPIView):
+    serializer_class = CreateUserSerializer
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        user = User.objects.get(username=user_data['username'])
+        current_site = get_current_site(request).domain
+        # add later based on a login url
+        # relativeLink = reverse('')
+        absurl = 'http://' + current_site
+        message = 'Welcome ' + user.username + ', click the link below to begin the writing on The Wall. \n' + absurl
+        data = {'message': message, 'to': user.email}
+        Util.send_email(data)
+
+        return Response(user_data, status=status.HTTP_201_CREATED)
 
 
 class ArticleListView(ListAPIView):
