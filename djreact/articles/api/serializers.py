f5ada8f387d3668ae4d13b2ce0d3d341dd781861
Alison Reilly <reillyamr@gmail.com>
2020-09-30 18:16:58 -0400

    add create user serializer

--- djreact/articles/api/serializers.py
+++ djreact/articles/api/serializers.py
@@ -4,6 +4,7 @@ from articles.models import Article
 
 from django.contrib.auth.models import User
 
+
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
@@ -14,3 +15,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email')
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
