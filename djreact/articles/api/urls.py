0eceb89386c6016a192f9ec09a9197c6a68512d3
Alison Reilly <reillyamr@gmail.com>
2020-10-02 17:11:29 -0400

    remove other example notes/urls

--- djreact/articles/api/urls.py
+++ djreact/articles/api/urls.py
@@ -10,7 +10,7 @@ from .views import (
     ArticleCreateView,
     ArticleUpdateView,
     ArticleDeleteView,
-   
+  
 )
 
 urlpatterns = [
@@ -19,17 +19,8 @@ urlpatterns = [
     path('<pk>', ArticleDetailView.as_view()),
     path('<pk>/update/', ArticleUpdateView.as_view()),
     path('<pk>/delete/', ArticleDeleteView.as_view()),
-    path('registration/account-email-verification-sent/', views.null_view, name='account_email_verification_sent'),
-    path('registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
-    path('registration/complete/$', views.complete_view, name='account_confirm_complete'),
-    path('password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.null_view, name='password_reset_confirm'),
+    # path('registration/account-email-verification-sent/', views.null_view, name='account_email_verification_sent'),
+    # path('registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
+    # path('registration/complete/$', views.complete_view, name='account_confirm_complete'),
+    # path('password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.null_view, name='password_reset_confirm'),
 ]
-
-# Previous short version for when auths aren't needed:
-
-# from articles.api.views import ArticleViewSet
-# from rest_framework.routers import DefaultRouter
-
-# router = DefaultRouter()
-# router.register(r'', ArticleViewSet, base_name='articles')
-# urlpatterns = router.urls
